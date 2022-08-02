1605acf98e0cecb9d487718c3e06383a3362faaa
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2022-08-02 14:34:04 +0300

    add descr meta

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,7 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta name="next.js app" content="some content" />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
